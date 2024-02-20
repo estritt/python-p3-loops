5a4eff588eafc413efb690344064117ad8e0acda
Genevieve Strittmatter <eastrittmatter@gmail.com>
2024-02-20 14:26:43 -0500

    completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        if i == 1:
+            print("Happy New Year!")
+        i -= 1
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [int ** 2 for int in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(100):
+        i += 1
+        if i % 3 == 0:
+            if i % 5 == 0:
+                print("FizzBuzz")
+            else:
+                print ("Fizz")
+        elif i % 5 == 0:
+            print ("Buzz")
+        else: 
+            print(i)
